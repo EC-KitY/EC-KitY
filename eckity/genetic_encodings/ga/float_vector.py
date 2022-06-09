23d0200b4f14deaa5325accc94116f4700ebbeff
itaitzruia4 <itaitz@post.bgu.ac.il>
2022-06-09 11:11:49 +0300

    fixed compilation errors in `genetic_encodings.ga` package

--- eckity/genetic_encodings/ga/float_vector.py
+++ eckity/genetic_encodings/ga/float_vector.py
@@ -2,15 +2,9 @@
 This module implements the vector class.
 """
 
-import numpy as np
-from numbers import Number
-from random import randint, uniform, random
+from random import uniform
 
-from eckity.base.utils import arity
-from eckity.genetic_encodings.gp.tree.utils import _generate_args
-
-from eckity.individual import Individual
-from eckity.genetic_encodings.gp.tree.functions import f_add, f_sub, f_mul, f_div
+from eckity.genetic_encodings.ga.vector_individual import Vector
 
 
 class FloatVector(Vector):
@@ -20,12 +14,13 @@ class FloatVector(Vector):
                  bounds=(0.0, 1.0)):
         super().__init__(fitness, length, bounds)
 
-        def get_random_number_in_bounds(self, index):
-            if type(bounds) == tuple:
-                return random.uniform(bounds[0], vector.get_bounds[1])
-            return random.uniform(bounds[index][0], vector.get_bounds[index][1])
+    def get_random_number_in_bounds(self, index):
+        if type(self.bounds) == tuple:
+            return uniform(self.bounds[0], self.bounds.get_bounds[1])
+        return uniform(self.bounds[index][0], self.bounds[index][1])
 
-        def get_random_number_with_gauss(self, index, gauss):
-            return index  # index
+    def get_random_number_with_gauss(self, index, gauss):
+        # TODO implement
+        return index  # index
 
 # end class float vector
