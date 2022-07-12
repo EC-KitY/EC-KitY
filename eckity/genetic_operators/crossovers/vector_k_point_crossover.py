6719e8c86b91920bb2f35b8bf96249520ccc7042
tomerhal <tomerhal@post.bgu.ac.il>
2022-07-12 11:33:44 +0300

    pydoc of k point crossover

--- eckity/genetic_operators/crossovers/vector_k_point_crossover.py
+++ eckity/genetic_operators/crossovers/vector_k_point_crossover.py
@@ -5,6 +5,25 @@ from eckity.genetic_operators.genetic_operator import GeneticOperator
 
 class VectorKPointsCrossover(GeneticOperator):
     def __init__(self, probability=1, arity=2, k=1, events=None):
+        """
+            Vector N Point Mutation.
+
+            Randomly chooses N vector cells and performs a small change in their values.
+
+            Parameters
+            ----------
+            probability : float
+                The probability of the mutation operator to be applied
+
+            arity : int
+                The number of individuals this mutation is applied on
+
+            k : int
+                Number of points to cut the vector for the crossover.
+
+            events: list of strings
+                Events to publish before/after the mutation operator
+        """
         self.individuals = None
         self.applied_individuals = None
         self.k = k
@@ -12,7 +31,19 @@ class VectorKPointsCrossover(GeneticOperator):
         super().__init__(probability=probability, arity=arity, events=events)
 
     def apply(self, individuals):
+        """
+        Attempt to perform the mutation operator
 
+        Parameters
+        ----------
+        individuals : list of individuals
+            individuals to perform crossover on
+
+        Returns
+        ----------
+        list of individuals
+            individuals after the crossover
+        """
         self.individuals = individuals
         self.points = sorted(sample(range(0, individuals[0].size()), self.k))
 
