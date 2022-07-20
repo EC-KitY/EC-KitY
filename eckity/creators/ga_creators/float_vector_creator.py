3fab9ed113271448342ce29c1815c8320106844c
itaitzruia4 <itaitz@post.bgu.ac.il>
2022-07-20 12:17:47 +0300

    fixed GA Integer and Float vector ctor arguments and updated version

--- eckity/creators/ga_creators/float_vector_creator.py
+++ eckity/creators/ga_creators/float_vector_creator.py
@@ -6,5 +6,10 @@ class GAFloatVectorCreator(GAVectorCreator):
     def __init__(self,
                  length=1,
                  gene_creator=None,
+                 bounds=(0.0, 1.0),
                  events=None):
-        super().__init__(length, gene_creator, FloatVector, events)
+        super().__init__(length=length,
+                         gene_creator=gene_creator,
+                         bounds=bounds,
+                         vector_type=FloatVector,
+                         events=events)
