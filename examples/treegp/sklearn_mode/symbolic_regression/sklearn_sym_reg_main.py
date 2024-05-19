b25b4961d119219d0f31065db0d0b19bf01e6e67
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-05-19 21:06:11 +0300

    improved readability of some examples

--- examples/treegp/sklearn_mode/symbolic_regression/sklearn_sym_reg_main.py
+++ examples/treegp/sklearn_mode/symbolic_regression/sklearn_sym_reg_main.py
@@ -22,7 +22,9 @@ from eckity.genetic_operators.selections.tournament_selection import TournamentS
 from eckity.sklearn_compatible.sk_regressor import SKRegressor
 from eckity.statistics.best_average_worst_statistics import BestAverageWorstStatistics
 from eckity.subpopulation import Subpopulation
-from eckity.termination_checkers.threshold_from_target_termination_checker import ThresholdFromTargetTerminationChecker
+from eckity.termination_checkers.threshold_from_target_termination_checker import (
+    ThresholdFromTargetTerminationChecker,
+)
 from eckity.sklearn_compatible.regression_evaluator import RegressionEvaluator
 
 
@@ -116,34 +118,39 @@ def main():
 
     # Initialize Simple Evolutionary Algorithm instance
     algo = SimpleEvolution(
-        Subpopulation(creators=RampedHalfAndHalfCreator(init_depth=(2, 4),
-                                                        terminal_set=terminal_set,
-                                                        function_set=function_set,
-                                                        erc_range=(-100, 100),
-                                                        bloat_weight=0.0001),
-                      population_size=1000,
-                      # user-defined fitness evaluation method
-                      evaluator=RegressionEvaluator(),
-                      # minimization problem (fitness is MAE), so higher fitness is worse
-                      higher_is_better=False,
-                      elitism_rate=0.05,
-                      # genetic operators sequence to be applied in each generation
-                      operators_sequence=[
-                          SubtreeCrossover(probability=0.9, arity=2),
-                          SubtreeMutation(probability=0.2, arity=1),
-                          ERCMutation(probability=0.05, arity=1)
-                      ],
-                      selection_methods=[
-                          # (selection method, selection probability) tuple
-                          (TournamentSelection(tournament_size=4, higher_is_better=False), 1)
-                      ]
-                      ),
+        Subpopulation(
+            creators=RampedHalfAndHalfCreator(
+                init_depth=(2, 4),
+                terminal_set=terminal_set,
+                function_set=function_set,
+                erc_range=(-100, 100),
+                bloat_weight=0.0001,
+            ),
+            population_size=1000,
+            # user-defined fitness evaluation method
+            evaluator=RegressionEvaluator(),
+            # minimization problem (fitness is MAE), so higher fitness is worse
+            higher_is_better=False,
+            elitism_rate=0.05,
+            # genetic operators sequence to be applied in each generation
+            operators_sequence=[
+                SubtreeCrossover(probability=0.9, arity=2),
+                SubtreeMutation(probability=0.2, arity=1),
+                ERCMutation(probability=0.05, arity=1),
+            ],
+            selection_methods=[
+                # (selection method, selection probability) tuple
+                (TournamentSelection(tournament_size=4, higher_is_better=False), 1)
+            ],
+        ),
         breeder=SimpleBreeder(),
         max_workers=1,
         max_generation=1000,
         # optimal fitness is 0, evolution ("training") process will be finished when best fitness <= threshold
-        termination_checker=ThresholdFromTargetTerminationChecker(optimal=0, threshold=0.01),
-        statistics=BestAverageWorstStatistics()
+        termination_checker=ThresholdFromTargetTerminationChecker(
+            optimal=0, threshold=0.01
+        ),
+        statistics=BestAverageWorstStatistics(),
     )
     # wrap the simple evolutionary algorithm with sklearn-compatible regressor
     regressor = SKRegressor(algo)
@@ -155,13 +162,15 @@ def main():
     regressor.fit(X_train, y_train)
 
     # check training set results
-    print(f'\nBest pure fitness over training set: {algo.best_of_run_.get_pure_fitness()}')
+    print(
+        f"\nBest pure fitness over training set: {algo.best_of_run_.get_pure_fitness()}"
+    )
 
     # check test set results by computing the MAE between the prediction result and the test set result
     test_score = mean_absolute_error(y_test, regressor.predict(X_test))
-    print(f'Test score: {test_score}')
+    print(f"Test score: {test_score}")
 
-    print(f'Total runtime: {time() - start_time} seconds.')
+    print(f"Total runtime: {time() - start_time} seconds.")
 
 
 if __name__ == "__main__":
