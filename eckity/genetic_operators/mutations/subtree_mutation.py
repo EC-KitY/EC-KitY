6dc419064460d4f728a9dfad044bd69bba2ef864
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-05-21 22:24:21 +0300

    push to run new workflows

--- eckity/genetic_operators/mutations/subtree_mutation.py
+++ eckity/genetic_operators/mutations/subtree_mutation.py
@@ -19,12 +19,17 @@ class SubtreeMutation(GeneticOperator):
         """
 
         for ind in individuals:
-            init_depth = (ind.init_depth[0], randint(ind.init_depth[0], ind.init_depth[1])) \
-                if self.init_depth is None \
+            init_depth = (
+                (ind.init_depth[0], randint(ind.init_depth[0], ind.init_depth[1]))
+                if self.init_depth is None
                 else self.init_depth
-            tree_creator = GrowCreator(init_depth=init_depth,
-                                       function_set=ind.function_set, terminal_set=ind.terminal_set,
-                                       erc_range=ind.erc_range)
+            )
+            tree_creator = GrowCreator(
+                init_depth=init_depth,
+                function_set=ind.function_set,
+                terminal_set=ind.terminal_set,
+                erc_range=ind.erc_range,
+            )
 
             # TODO refactor dummy individual creation, only the tree should be generated
             subtree_individual = tree_creator.create_individuals(1, None)[0]
