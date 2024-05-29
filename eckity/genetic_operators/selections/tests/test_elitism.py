101d43a18b3cbc2af642c064f35456c1f25903a6
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-05-29 20:36:41 +0300

    Add warning for ElitismSelection with zero elites

--- eckity/genetic_operators/selections/tests/test_elitism.py
+++ eckity/genetic_operators/selections/tests/test_elitism.py
@@ -1,17 +1,25 @@
-import pytest
-
+import logging
 from eckity.fitness.simple_fitness import SimpleFitness
-from eckity.genetic_operators.selections.elitism_selection import ElitismSelection
+from eckity.genetic_operators.selections.elitism_selection import (
+    ElitismSelection,
+)
 from eckity.genetic_encodings.ga.bit_string_vector import BitStringVector
 
-class TestElitism:
-	def test_selected_by(self):
-		elitism = ElitismSelection(2, higher_is_better=True)
-		inds = [
-			BitStringVector(SimpleFitness(1), length=4),
-			BitStringVector(SimpleFitness(2), length=4)
-        ]
-		selected = elitism.select(inds, [])
-		chosen = selected[0]
-		assert chosen.selected_by == ['ElitismSelection']
-		assert chosen.cloned_from == [inds[1].id]
+
+def test_selected_by():
+    elitism = ElitismSelection(2, higher_is_better=True)
+    inds = [
+        BitStringVector(SimpleFitness(1), length=4),
+        BitStringVector(SimpleFitness(2), length=4),
+    ]
+    selected = elitism.select(inds, [])
+    chosen = selected[0]
+    assert chosen.selected_by == ["ElitismSelection"]
+    assert chosen.cloned_from == [inds[1].id]
+
+
+def test_zero_elites_warnning(caplog):
+    with caplog.at_level(logging.WARNING):
+        ElitismSelection(num_elites=0)
+    assert len(caplog.records) == 1
+    assert "Elitism selection with 0 elites is ineffective." in caplog.text
