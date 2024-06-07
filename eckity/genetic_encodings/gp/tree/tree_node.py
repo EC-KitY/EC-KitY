f43422bc5aadde42ff2e49e668312fea2d0b0c58
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-06-07 17:05:15 +0300

    replace children_types with automatic type checking

--- eckity/genetic_encodings/gp/tree/tree_node.py
+++ eckity/genetic_encodings/gp/tree/tree_node.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, get_type_hints
 
 
 class TreeNode:
@@ -11,9 +11,6 @@ class TreeNode:
         node type
     value : _type_
         node value
-    children_types: List[type]
-        function argument types
-        for instance, f_add GPNode should have children_types = [int, int]
     children: List[GPNode]
         list of children nodes
     """
@@ -21,7 +18,6 @@ class TreeNode:
     def __init__(self, node_type, value) -> None:
         self.children: List[TreeNode] = []
         self.node_type: type = None
-        self.children_types: List[type] = []  # function argument types
         self.value: self.node_type = None
 
         if self.node_type is None:
@@ -34,4 +30,21 @@ class TreeNode:
             return self.value
 
     def add_child(self, child: "TreeNode"):
+        if type(self.value) is not callable:
+            raise ValueError("Cannot add child to terminal node.")
+
+        # Check if child is of the correct type
+        func_types = list(get_type_hints(self.value).values())
+        child_idx = len(self.children)
+
+        # Check if there are too many children
+        if child_idx >= len(func_types):
+            raise ValueError(f"Too many children for function {self.value}.")
+
+        # Check if the child is of the correct type
+        if not isinstance(child.value, func_types[child_idx]):
+            raise ValueError(
+                f"Child {child_idx} of function {self.value} "
+                f"should be of type {func_types[child_idx]}."
+            )
         self.children.append(child)
