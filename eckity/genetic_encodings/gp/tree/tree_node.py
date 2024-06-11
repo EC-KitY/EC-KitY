c6216d0fe829142ec6667536231e488127aa167c
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-06-11 14:27:42 +0300

    Add FunctionNode and TerminalNode subclasses

--- eckity/genetic_encodings/gp/tree/tree_node.py
+++ eckity/genetic_encodings/gp/tree/tree_node.py
@@ -1,7 +1,11 @@
-from typing import List, Callable, get_type_hints
+from abc import ABC, abstractmethod
+from types import BuiltinFunctionType, FunctionType
+from typing import Any, Dict, List, Union, get_type_hints
+
+from overrides import override
 
 
-class TreeNode:
+class TreeNode(ABC):
     """
     GP Node
 
@@ -15,36 +19,57 @@ class TreeNode:
         list of children nodes
     """
 
-    def __init__(self, node_type, value) -> None:
-        self.children: List[TreeNode] = []
-        self.node_type: type = None
-        self.value: self.node_type = None
+    def __init__(self, node_type) -> None:
+        self.node_type: type = node_type
 
-        if self.node_type is None:
-            self.node_type = type(self.value)
-
+    @abstractmethod
     def apply(self):
-        if isinstance(self.value, Callable):
-            return self.value(*[child.apply() for child in self.children])
-        else:
-            return self.value
+        """
+        Returns the value of this node
+        Return type must match the type of this node
+        """
+        pass
 
-    def add_child(self, child: "TreeNode"):
-        if type(self.value) is not Callable:
-            raise ValueError("Cannot add child to terminal node.")
 
+class FunctionNode(TreeNode):
+    def __init__(
+        self, function: Union[FunctionType, BuiltinFunctionType]
+    ) -> None:
+        super().__init__(type(function))
+        self.function = function
+        self.children: List[TreeNode] = []
+
+    @override
+    def apply(self):
+        return self.function(*[child.apply() for child in self.children])
+
+    def add_child(self, child: TreeNode):
         # Check if child is of the correct type
-        func_types = list(get_type_hints(self.value).values())
+        params_types: Dict = get_type_hints(self.function)
+        func_types = list(params_types.values())
         child_idx = len(self.children)
 
         # Check if there are too many children
         if child_idx >= len(func_types):
-            raise ValueError(f"Too many children for function {self.value}.")
+            raise ValueError(
+                f"Too many children for function {self.function}."
+            )
 
         # Check if the child is of the correct type
-        if not isinstance(child.value, func_types[child_idx]):
+        expected_type = func_types[child_idx]
+        if child.node_type != expected_type:
             raise ValueError(
-                f"Child {child_idx} of function {self.value} "
-                f"should be of type {func_types[child_idx]}."
+                f"Expected Child {child_idx} of function {self.function} "
+                f"to be of type {expected_type}. Got {child.node_type}"
             )
         self.children.append(child)
+
+
+class TerminalNode(TreeNode):
+    def __init__(self, value: Any) -> None:
+        super().__init__(type(value))
+        self.value = value
+
+    @override
+    def apply(self):
+        return self.value
