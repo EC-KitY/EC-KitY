9a9ac74358e95cdbabd500ea4b0c7c0c633db330
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-06-18 14:11:27 +0300

    add optional unique tournaments parameter in TournamentSelection

--- eckity/genetic_operators/selections/tournament_selection.py
+++ eckity/genetic_operators/selections/tournament_selection.py
@@ -1,29 +1,71 @@
+import random
+from overrides import override
 
-from random import choices
-
-from eckity.genetic_operators.selections.selection_method import SelectionMethod
+from eckity.genetic_operators.selections.selection_method import (
+    SelectionMethod,
+)
 
 
 class TournamentSelection(SelectionMethod):
-    def __init__(self, tournament_size, higher_is_better=False, events=None):
+    def __init__(
+        self,
+        tournament_size,
+        higher_is_better=False,
+        unique=False,
+        events=None,
+    ):
+        """
+        Tournament Selection method.
+        In this method, small groups (known as tournaments) are created from
+        randomly chosen individuals. The individual(s) with the best fitness
+        scores are selected to reproduce the next generation.
+
+        Parameters
+        ----------
+        tournament_size : int
+            Size of each tournament.
+            larger tournaments focus on exploitation,
+            while small tournaments focus on exploration.
+        higher_is_better : bool, optional
+            is higher fitness better or worse, by default False
+        unique : bool, optional
+            whether tournaments can contain multiple copies of the same
+            individual, by default False
+        events : List[str], optional
+            selection events, by default None
+        """
         super().__init__(events=events, higher_is_better=higher_is_better)
         self.tournament_size = tournament_size
+        self.unique = unique
 
+    @override
     def select(self, source_inds, dest_inds):
-        # the selection should add len(source_inds) individuals to dest_inds, so the required number of tournaments
-        # is the size of source individuals divided by the number of winners per tournament
-        # n_tournaments = len(source_inds) // self.operator_arity
+        """
+        The selection should add len(source_inds) individuals to dest_inds,
+        so the required number of tournaments is the size of source
+        individuals divided by the number of winners per tournament.
+        `n_tournaments = len(source_inds) // self.operator_arity`
+        """
         n_tournaments = (len(source_inds) - len(dest_inds)) // self.arity
 
+        """
+        Select the appropriate tournament creation function.
+        `random.choices` selects k elements with replacements,
+        while `random.sample` selects k unique elements
+        """
+        sel_func = random.choices if self.unique else random.sample
+
         # create all tournaments beforehand
-        tournaments = [choices(source_inds, k=self.tournament_size) for _ in range(n_tournaments)]
+        tournaments = [
+            sel_func(source_inds, k=self.tournament_size)
+            for _ in range(n_tournaments)
+        ]
 
         # pick the winner of each tournament and add all winners to dest_inds
         winners = [self._pick_tournament_winner(tour) for tour in tournaments]
         dest_inds.extend(winners)
 
         self.selected_individuals = dest_inds
-        # self.publish("after_selection")   # TODO this already publishes 'after_operator' event
 
         return dest_inds
 
