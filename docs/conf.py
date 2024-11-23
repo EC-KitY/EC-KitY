a5b1a49267caf86877375fe06b79673692238bbf
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-11-23 19:20:10 +0200

    remove old doc files

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+from eckity import version
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'EC-KitY'
+copyright = '2023, Sipper'
+author = 'Moshe Sipper'
+
+release = "0.3.4"
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Welcome to EC-KitY documentation!
+===================================
+
+**EC-KitY** is a Python library for Evolutionary Computation with seamless Machine Learning integration.
+
+Check out the :doc:`usage` section for further information, including
+how to :ref:`installation` the project.
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   usage
+   api
